5d9a256bbdba4cb53f10683050299a6e53e12b20
UnnamedShadow <u60_matdom_waw@technischools.com>
2022-12-22 19:21:27 +0100

    removed debug info

--- scriptRblog/script/main.js
+++ scriptRblog/script/main.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-  console.log("Running onload");
   const navbar = document.getElementById("navbar");
   const iframe = document.getElementsByTagName("iframe")[0];
   for (var i = 0; i < navbar.childElementCount; i++) {
